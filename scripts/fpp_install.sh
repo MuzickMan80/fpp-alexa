482672e591f5871c9f6e3caa3111d25709fdabd5
Dale Peterson <dale@megndale.com>
2020-12-06 11:29:08 -0600

    Add exe permissions to scripts

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # fpp-alexa install script
+echo "Running fpp-alexa install script..."
+
 sudo apt-get update
 sudo apt-get -f install nodejs npm flask flask-ask
 sudo npm install -g localtunnel
 
+echo "Done installing fpp-alexa"
